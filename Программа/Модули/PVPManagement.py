e52715971afb2bc6ceafaff09858c7ebef3e84a4
ChazGrant <ilya20012209@yandex.ru>
2024-03-23 21:39:00 +0300

    Добавлены файлы модулей

---
+++ Программа/Модули/PVPManagement.py
@@ -0,0 +1,27 @@
+class PVP:
+    def __init__(self):
+        ...
+
+    def findArena(self, arena_type: int):
+        """
+            Ищет подходящую арену, с указанным типом(2vs2, 4vs4, 8vs1)
+        """
+        ...
+
+    def findPlayerForPVP(self):
+        """
+            Ищет подходящего игрока для дуэли
+        """
+        ...
+
+    def acceptDuelRequest(self):
+        """
+            Принимает запрос на дуэль от другого игрока
+        """
+        ...
+
+    def invadeToAnotherWorld(self):
+        """
+            Игрок вторгается в мир другого игрока
+        """
+        ...
